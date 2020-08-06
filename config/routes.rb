cf9956295f0eff4b5d18e6795b5db6778be35df1
Tyler Hall <tylerhall12@outlook.com>
2020-08-06 15:10:50 +1000

    Removed routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
+
+  get "/", to: "profiles#home", as: "root"
+  get "/how_this_works", to: "profiles#how_this_works", as: "help"
+
   get "/404", to:"errors#not_found"
   get "/422", to:"errors#unacceptable"
   get "/500", to:"errors#internal_error"
-
-  get 'categories/index'
-  get 'categories/show'
 
   resources :listings
 
   resources :profiles do
     post "reviews", to: "reviews#create"
   end
-
-  get "/", to: "profiles#home", as: "root"
-  get "/how_this_works", to: "profiles#how_this_works", as: "help"
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
