bffff176b655c69ac6c15973d806e3051d89cef8
Tyler Hall <tylerhall12@outlook.com>
2020-08-09 12:03:07 +1000

    Added styling to profile index

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,13 +29,13 @@ require 'populator'
     )
     user.profile.save!
 
-    # 4.times do
-    #     user.profile.listings = Listing.create(
-    #         title: "Photography Listing",
-    #         description: "Photography Listing",
-    #         price: 300,
-    #         category_id: rand(1..6)
-    #     )
-    #     user.profile.listings.save!
-    # end
+    4.times do
+        user.profile.listings = Listing.create(
+            title: "Photography Listing",
+            description: "Photography Listing",
+            price: 300,
+            category_id: rand(1..6)
+        )
+        user.profile.listings.save!
+    end
 end
